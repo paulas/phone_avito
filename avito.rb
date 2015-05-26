2522ae72a88ea51e3d47cd572ed5ad798d547bd9
NovikovRoman <service.for.roman@gmail.com>
2015-05-26 23:15:58 +0300

    Номер из png в текстовом формате

--- avito.rb
+++ avito.rb
@@ -4,12 +4,15 @@ require 'curb'
 require 'chunky_png'
 
 class Avito
-	attr_accessor :url, :path
+	attr_accessor :url, :path, :number_phone
 	USER_AGENT = 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36'
 	HOST = 'www.avito.ru'
+	COUNTS_PIXEL = [ 44, 10, 38, 43, 28, 41, 45, 24, 53, 47 ]
 	def initialize(url)
 		self.url = url
 		@path = 'phones/'
+		@id_phone = nil
+		@number_phone = ''
 	end
 
 	def url=(url)
@@ -24,11 +27,57 @@ class Avito
 	end
 
 	def get_phone(path=nil)
+		self.get_phone_png(path) unless @id_phone
+
+		get_mask_png
+
+		# разобьем по цифрам
+		@ar_digits = []
+		new_digit = true
+		num = 0
+		@ar_mask_png.each_with_index do |row|
+			sum = row.inject { |sum, x| sum + x }
+			next if (sum == 1 and not new_digit)
+			if sum < 1
+				unless new_digit
+					num += 1
+					new_digit = true
+				end
+			else
+				new_digit = false
+				@ar_digits.push [] unless @ar_digits[num].is_a? Array
+				@ar_digits[num].push(row)
+			end
+		end
+		# повернем все цифры
+		@ar_digits.each_with_index do |digit, index_digit|
+			ar = []
+			r = digit[0].length
+			c = digit.length
+			(0...r).collect{ ar.push Array.new(c) }
+			(0...c).collect do |col|
+				(0...r).collect do |row|
+					ar[row][col] = digit[col][row]
+				end
+			end
+			# удалим лишние строки
+			new_ar = []
+			ar.each do |row|
+				new_ar.push row unless row.inject { |sum, x| sum + x } < 2
+			end
+			@ar_digits[index_digit] = new_ar
+		end
+
+		get_phone_text
+
+	end
+
+	def get_phone_png(path=nil)
 		self.path(path) if path
-		id = url.strip.scan(/_(\d+)$/i)
-		raise ArgumentError, 'Неверный URL' unless id.is_a? Array
-		id = id[0][0].to_i
-		cookie_file = "cookie_#{id}"
+		id_scan = url.strip.scan(/_(\d+)$/i)
+		raise ArgumentError, 'Неверный URL' unless id_scan.is_a? Array
+		@id_phone = id_scan[0][0].to_i
+		cookie_file = "cookie_#{@id_phone}"
 		curl = Curl::Easy.new do |c|
 			c.url = url
 			c.useragent = USER_AGENT
@@ -49,8 +98,8 @@ class Avito
 		curl.close
 		code_phone = html.scan(/avito\.item\.phone\s*=\s*('|")(.+?)('|")/sui)
 		raise StandardError, 'Не найден код телефона на странице' unless code_phone.is_a? Array
-		code = get_code(id, code_phone[0][1])
-		url_img = "https://www.avito.ru/items/phone/#{id}?pkey=#{code}"
+		code = get_code(@id_phone, code_phone[0][1])
+		url_img = "https://www.avito.ru/items/phone/#{@id_phone}?pkey=#{code}"
 		curl = Curl::Easy.new do |c|
 			c.url = url_img
 			c.useragent = USER_AGENT
@@ -70,7 +119,7 @@ class Avito
 		end
 		curl.perform
 		# save png
-		filename = "#{@path}#{id.to_s}.png"
+		filename = "#{@path}#{@id_phone}.png"
 		file_png = File.new filename, mode='w'
 		file_png.write curl.body
 		file_png.close
@@ -92,71 +141,76 @@ class Avito
 		end
 		result
 	end
+
+	def get_phone_text
+		@number_phone = ''
+		@ar_digits.each do |digit|
+			if digit.size > 3
+				count_pixel = 0
+				digit.each do |row|
+					count_pixel += row.inject { |sum, x| sum + x }
+				end
+				n = COUNTS_PIXEL.index(count_pixel).to_s
+				@number_phone += n ? n : 'x'
+			end
+		end
+		@number_phone
+	end
+
+	def get_mask_png
+		png = ChunkyPNG::Image.from_file "#{@path}#{@id_phone}.png"
+		w = png.width
+		h = png.height
+		@ar_mask_png = (0...w).collect { Array.new(h) }
+		w.times do |x|
+			h.times do |y|
+				@ar_mask_png[x][y] = (png[x, y] == 4294967040) ? 0 : 1
+				# printf @ar_mask_png[x][y] == 1 ? '#' : ' '
+			end
+			# printf "\n"
+		end
+	end
+
 end
 
 =begin
 avito = Avito.new '/novocheboksarsk/zapchasti_i_aksessuary/turbina_gt1852v_garret_559224246'
-p avito.get_phone
+p avito.get_phone_png
 =end
 
-png = ChunkyPNG::Image.from_file 'phones/559224246.png'
-w = png.width
-h = png.height
+avito = Avito.new 'https://www.avito.ru/zalukokoazhe/rezume/ischu_rabotu_565846659'
+avito.get_phone
+p avito.number_phone
 
-ar_png = (0...w).collect { Array.new(h) }
+=begin
+0 - 44
+1 - 10
+2 - 38
+3 - 43
+4 - 28
+5 - 41
+6 - 45
+7 - 24
+8 - 53
+9 - 47
+=end
 
-w.times do |x|
-	h.times do |y|
-		ar_png[x][y] = (png[x, y] == 4294967040) ? 0 : 1
-	end
-end
-
-# разобьем по цифрам
-digits = []
-new_digit = true
-num = 0
-ar_png.each_with_index do |row|
-	if row.inject { |sum, x| sum + x } < 1
-		unless new_digit
-			num += 1
-			new_digit = true
-		end
-	else
-		new_digit = false
-		digits.push [] unless digits[num].is_a? Array
-		digits[num].push(row)
-	end
-end
-
-# повернем все цифры
-digits.each_with_index do |digit, index_digit|
-	ar = []
-	r = digit[0].length
-	c = digit.length
-	(0...r).collect{ ar.push Array.new(c) }
-
-	(0...c).collect do |col|
-		(0...r).collect do |row|
-			ar[row][col] = digit[col][row]
-		end
-	end
-	# удалим лишние строки
-	new_ar = []
-	ar.each do |row|
-		new_ar.push row unless row.inject { |sum, x| sum + x } < 2
-	end
-	digits[index_digit] = new_ar
-end
-
-digits.each do |digit|
+=begin
+@ar_digits.each do |digit|
 	if digit.size > 3
+		count_pixel = 0
 		digit.each do |row|
+			count_pixel += row.inject { |sum, x| sum + x }
 			row.each do |v|
 				printf v == 1 ? '#' : ' '
 			end
-			p ''
+			printf "\n"
 		end
-		p ' '*10
+		printf "\n\n"
+		p count_pixel
+		printf "\n\n"
 	end
 end
+=end
+
 
